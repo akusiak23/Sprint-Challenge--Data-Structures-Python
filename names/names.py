db4a1bb3e0a45951f6863efdbf81992e898e36e5
Arkadiusz Kusiak <akusiak23@gmail.com>
2019-03-01 12:25:44 -0600

    names

--- names/names.py
+++ names/names.py
@@ -10,13 +10,23 @@ f = open('names_2.txt', 'r')
 names_2 = f.read().split("\n")  # List containing 10000 names
 f.close()
 
+dict_1 = {}
 duplicates = []
+
+# Original Code
+# for name_1 in names_1:
+#     for name_2 in names_2:
+#         if name_1 == name_2:
+#             duplicates.append(name_1)
+
 for name_1 in names_1:
-    for name_2 in names_2:
-        if name_1 == name_2:
-            duplicates.append(name_1)
+    dict_1[name_1] = True
+
+for name_2 in names_2:
+    if dict_1.get(name_2, None):
+        duplicates.append(name_2)
+
 
 end_time = time.time()
-print (f"{len(duplicates)} duplicates:\n\n{', '.join(duplicates)}\n\n")
-print (f"runtime: {end_time - start_time} seconds")
-
+print(f"{len(duplicates)} duplicates:\n\n{', '.join(duplicates)}\n\n")
+print(f"runtime: {end_time - start_time} seconds")
